7cbc2e54ae8994af2254f5bb128f70be63d30340
mao <Mstarfish@163.com>
2018-02-12 23:14:51 -0500

    works

--- parser.py
+++ parser.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python
 import re
 import os
-import sys
 
 Path = "data/2015-03-18/"
 filelist = os.listdir(Path)
-sys.stdout.write("[")
 i = 0
+dic = {}
 for file in filelist:
     if file.endswith(".html") and file.startswith("lot"): 
         fh = open(Path+file, "r")
+        num = 1
         line = fh.readline()
         while line:
-			reobj = re.search( r'<h2>(.*?)( \(.*\))*</h2>', line)
-			if reobj:
-				if i:
-					sys.stdout.write(", ")
-				sys.stdout.write('\''+reobj.group(1)+'\'')
-				i = 1
-				break
+			if num == 1:
+				reobj = re.search( r'<h2>(.*?)( \(.*\))*</h2>', line)
+				if reobj:
+					artist = '\''+reobj.group(1)+'\''
+					if not dic.has_key(artist):
+						dic[artist] = [];
+					num += 1
+			elif num == 2:
+				reobj = re.search( r'<h3>(.*?)</h3>', line)
+				if reobj:
+					dic[artist].append('\''+reobj.group(1)+'\'')
+					num += 1
+					break
 			line = fh.readline()
         fh.close()
-sys.stdout.write("]\n")
+
+
+
+print "["
+for artist in dic.keys():
+	print '\t{'
+	print '\t\tartist: '+artist+','
+	print '\t\tworks: ['+', '.join(dic[artist])+'],'
+	print '\t},'
+print "]"
  # exit;
