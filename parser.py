78b7b0fd18466073ab3426a3e8bfc51205d4b89c
mao <Mstarfish@163.com>
2018-02-13 00:31:14 -0500

    separating the currency from the amount

--- parser.py
+++ parser.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 import re
 import os
+import locale
+
+locale.setlocale( locale.LC_ALL, 'en_US.UTF-8' ) 
 
 # class Artwork:
 # 	def __init__(self, title, price):
 # 		self.title = title
 # 		self.price = price
 
-Path = "data/2015-03-18/"
+Path = "data/2017-12-20/"
 filelist = os.listdir(Path)
 dic = {}
+dicValue = {}
 for file in filelist:
     if file.endswith(".html") and file.startswith("lot"): 
         fh = open(Path+file, "r")
@@ -17,11 +21,12 @@ for file in filelist:
         line = fh.readline()
         while line:
 			if num == 1:
-				reobj = re.search( r'<h2>(.*?)( \(.*\))*</h2>', line)
+				reobj = re.search( r'<h3 class=\'artist\'>(.*?)( \(.*\))*</h3>', line)
 				if reobj:
 					artist = '\''+reobj.group(1)+'\''
 					if not dic.has_key(artist):
 						dic[artist] = [];
+						dicValue[artist] = 0;
 					num += 1
 			elif num == 2:
 				reobj = re.search( r'<h3>(.*?)</h3>', line)
@@ -29,10 +34,15 @@ for file in filelist:
 					title = '\''+reobj.group(1)+'\''
 					num += 1
 			elif num == 3:
-				reobj = re.search( r'<div>(.*?) (.*?)</div>', line)
+				reobj = re.search( r'<div><span class=\'currency\'>(.*?)</span><span>(.*?)</span></div>', line)
 				if reobj:
 					currency = '\''+reobj.group(1)+'\''
-					amount = '\''+reobj.group(2)+'\''
+					value = locale.atoi(reobj.group(2))
+					if currency == '\'GBP\'':
+						value *= 1.34
+						currency = '\'USD\''
+					dicValue[artist] += value;
+					amount = '\''+"{:,}".format(value)+'\''
 					dic[artist].append({'title': title, 'currency': currency, 'amount': amount})
 					break
 			line = fh.readline()
@@ -42,6 +52,7 @@ print "["
 for artist in dic.keys():
 	print '\t{'
 	print '\t\tartist: '+artist+','
+	print '\t\ttotalValue: \'USD '+"{:,}".format(dicValue[artist])+'\','
 	print '\t\tworks:',
 	for work in dic[artist]:
 		print '['
